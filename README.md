8b3fa54f4a59c5244e404e7588e91f712b381ef3
Eric Johnson <eric.git@iijo.org>
2016-08-05 10:30:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::JSON::Path - It's new $module
+
+# SYNOPSIS
+
+    use App::JSON::Path;
+
+# DESCRIPTION
+
+App::JSON::Path is ...
+
+# LICENSE
+
+Copyright (C) Eric Johnson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Eric Johnson <eric.git@iijo.org>
---
+++ lib/App/JSON/Tools.pm
@@ -0,0 +1,39 @@
+package App::JSON::Path;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::JSON::Path - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::JSON::Path;
+
+=head1 DESCRIPTION
+
+App::JSON::Path is ...
+
+=head1 LICENSE
+
+Copyright (C) Eric Johnson.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Eric Johnson E<lt>eric.git@iijo.orgE<gt>
+
+=cut
+
